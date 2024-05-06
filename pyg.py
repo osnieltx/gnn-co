28f7bf4af1b1c5e1244007924680fb8f9341d375
Osniel Teixeira <osniel.teixeira@itegra.com.br>
2024-05-06 14:11:23 -0300

    moving things around

---
+++ pyg.py
@@ -0,0 +1,44 @@
+import tqdm
+import torch
+
+# torch geometric
+try:
+    import torch_geometric
+except ModuleNotFoundError:
+    print('Installing torch_geometric')
+    import subprocess
+
+    # Installing torch geometric packages with specific CUDA+PyTorch version.
+    # See https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html for details
+    if torch.version.cuda is not None:
+        TORCH = torch.__version__.split('+')[0]
+        CUDA = 'cu' + torch.version.cuda.replace('.','')
+        packages = ['torch-scatter', 'torch-sparse', 'torch-cluster', 'torch-spline-conv']
+        for p in tqdm(packages, unit='pkg'):
+            bashCommand = f"python3.8 -m pip install --no-index {p} -f https://pytorch-geometric.com/whl/torch-{TORCH}+{CUDA}.html"
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+        # bashCommand = f"python3.8 -m pip install torch_geometric"
+        # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+
+    else:
+        # bashCommand = f"pip install torch_geometric"
+        # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+        # print(f'{output}, {error}')
+
+        bashCommand = f"pip install  torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.2.0+cpu.html"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+    # bashCommand = f"python3.8 -m pip install torch_geometric"
+    # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    # output, error = process.communicate()
+    # print(f'{output}, {error}')
+
+    print(f'{output=}, {error=}')
+    import torch_geometric
+
+import torch_geometric.nn as geom_nn
