54cdd5c7adac653bab54cfda361a4e8f3c80adaf
Osniel Teixeira <osnielteixeira@icloud.com>
2024-06-02 08:14:01 -0300

    removing MLPNN

--- models.py
+++ models.py
@@ -83,50 +83,19 @@ class GNNModel(nn.Module):
         return probability_maps
 
 
-class MLPModel(nn.Module):
-    def __init__(self, c_in, c_hidden, c_out, num_layers=2, dp_rate=0.0):
-        """
-        Inputs:
-            c_in - Dimension of input features
-            c_hidden - Dimension of hidden features
-            c_out - Dimension of the output features. Usually number of classes in classification
-            num_layers - Number of hidden layers
-            dp_rate - Dropout rate to apply throughout the network
-        """
-        super().__init__()
-        layers = []
-        in_channels, out_channels = c_in, c_hidden
-        for l_idx in range(num_layers - 1):
-            layers += [
-                nn.Linear(in_channels, out_channels),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dp_rate)
-            ]
-            in_channels = c_hidden
-        layers += [nn.Linear(in_channels, c_out)]
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x, *args, **kwargs):
-        """
-        Inputs:
-            x - Input features per node
-        """
-        return self.layers(x)
-
-
 NodeFowardResult = namedtuple('NodeFowardResult',
                               ['loss', 'acc', 'aon', 'mvc_score'])
 
 class NodeLevelGNN(pl.LightningModule):
-    def __init__(self, model_name, batch_size=None, **model_kwargs):
+    def __init__(self, batch_size=None, **model_kwargs):
         super().__init__()
         # Saving hyperparameters
         self.save_hyperparameters()
 
-        if model_name == "MLP":
-            self.model = MLPModel(**model_kwargs)
-        else:
-            self.model = GNNModel(**model_kwargs)
+        # if model_name == "MLP":
+        #     self.model = MLPModel(**model_kwargs)
+        # else:
+        self.model = GNNModel(**model_kwargs)
         self.loss_module = nn.BCEWithLogitsLoss()
 
         self.log = partial(self.log, batch_size=batch_size)
