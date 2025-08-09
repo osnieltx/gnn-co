e2d5706dbc09ae601f59e7bd190bf6454ddd2a79
Osniel Teixeira <osnielteixeira@icloud.com>
2025-08-09 08:25:28 -0300

    setting gurobi license id env var to int

--- gurobi_manager.py
+++ gurobi_manager.py
@@ -3,5 +3,5 @@ import os
 options = {
     "WLSACCESSID": os.environ.get("WLSACCESSID"),
     "WLSSECRET": os.environ.get("WLSSECRET"),
-    "LICENSEID": os.environ.get("LICENSEID"),
+    "LICENSEID": int(os.environ.get("LICENSEID"), 0) or None,
 }
